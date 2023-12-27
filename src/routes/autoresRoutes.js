f5b2822312b9fd798bedc276909489563064cc2c
RenanOshiroDev <renanoshiro.dev@gmail.com>
2023-12-27 15:50:29 -0300

    fix: :bug: fix: erro de linter

--- src/routes/autoresRoutes.js
+++ src/routes/autoresRoutes.js
@@ -2,6 +2,7 @@ import express from 'express';
 import AutoresController from '../controllers/autoresController.js';
 const router = express.Router();
 
+
 router
   .get('/autores', AutoresController.listarAutores)
   .get('/autores/:id', AutoresController.listarAutorPorId)
