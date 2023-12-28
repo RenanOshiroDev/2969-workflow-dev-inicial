70ca15e23d58c6798344195ead467aa72aedebde
RenanOshiroDev <renanoshiro.dev@gmail.com>
2023-12-28 11:36:05 -0300

    fix: :bug: add: fixando bug de teste

--- src/routes/autoresRoutes.js
+++ src/routes/autoresRoutes.js
@@ -2,7 +2,6 @@ import express from 'express';
 import AutoresController from '../controllers/autoresController.js';
 const router = express.Router();
 
-
 router
   .get('/autores', AutoresController.listarAutores)
   .get('/autores/:id', AutoresController.listarAutorPorId)
